033dc0abfdb4bc5592e1a2ee50ca41b84de90494
Ram kumar <rk16013093@gmail.com>
2021-04-21 15:54:13 +0530

    providing function to middleware so that it also available for verification of refresh token

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -1,8 +1,13 @@
 import * as jwt from "jsonwebtoken";
 import {UserPayload} from "../controllers/AuthController";
 
+const verifyToken = (token: any, secret: any) => {
+    return jwt.verify(token, secret as string) as UserPayload;
+};
+
 export default (req: any, res: any, next: any) => {
     const authHeader = req.get("Authorization");
+    const refreshHeader = req.get("x-refresh-token");
     let decodedToken: UserPayload;
 
     if (!authHeader) {
@@ -10,13 +15,18 @@ export default (req: any, res: any, next: any) => {
         return next();
     }
     const token = authHeader.split(" ")[1];
+    //console.log("token is ",token)
     if (!token || token === "") {
         req.isAuth = false;
         return next();
     }
 
     try {
-        decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as UserPayload;
+        if (refreshHeader) {
+            decodedToken = verifyToken(token, process.env.JWT_REFRESH_SECRET);
+        } else {
+            decodedToken = verifyToken(token, process.env.JWT_ACCESS_SECRET);
+        }
         console.log("Decoded token is: ", decodedToken);
     } catch (err) {
         console.log(err);

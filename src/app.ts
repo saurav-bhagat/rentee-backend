249c5a7a5e8b23bc755cc08bfd7fdef210a1d690
Ram kumar <rk16013093@gmail.com>
2021-04-23 23:18:19 +0530

    mongodb setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import dotenv from "dotenv";
 import express from "express";
-import * as Sequelize from "sequelize";
+import mongoose from "mongoose";
 
 import routes from "./routes";
 import isAuth from "./middleware/is-auth";
@@ -23,22 +23,17 @@ app.get("/", (req, res) => {
 });
 
 // database setup
-const dbConfig = new Sequelize.Sequelize(
-    process.env.DB || "",
-    process.env.DB_USER_NAME || "",
-    process.env.DB_PASSWORD || "",
-    {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT as any,
-        dialect: "mysql",
-    }
-);
 
-dbConfig
-    .authenticate()
-    .then(() => console.log("connected to db"))
+mongoose
+    .connect(process.env.DATABASE_URI as string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("db Connection is successfull");
+    })
     .catch((err) => {
-        console.log(`Error connection to database: ${err}`);
+        console.log(err);
     });
 
 app.listen(PORT, () => {

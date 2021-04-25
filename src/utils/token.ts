7ee9465466afd4f0b19c6965192d83554b224efc
Ram kumar <rk16013093@gmail.com>
2021-04-25 13:17:39 +0530

    tokens for auth

---
+++ src/utils/token.ts
@@ -0,0 +1,33 @@
+import {sign, verify} from "jsonwebtoken";
+
+const createToken = (userId: any, jwtSecret: string, expireTime: string) => {
+    return new Promise((resolve, reject) => {
+        const payload = {
+            user: userId,
+        };
+        const options = {
+            expiresIn: expireTime,
+            issuer: "rentee.com",
+            // audience: userId,
+        };
+        sign(payload, jwtSecret, options, (err, token) => {
+            if (err) {
+                console.log(err.message);
+                reject(err);
+                return;
+            }
+            resolve(token);
+        });
+    });
+};
+
+export const verifyRefreshToken = (refreshToken: string) => {
+    return new Promise((resolve, reject) => {
+        verify(refreshToken, process.env.JWT_REFRESH_SECRET as string, (err, payload) => {
+            if (err) return reject("err while  authorization");
+            resolve(payload);
+        });
+    });
+};
+
+export default createToken;

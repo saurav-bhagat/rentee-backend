aeb57472c632ee9c16e0d71e332fb3ff11ef3b73
Ram kumar <rk16013093@gmail.com>
2021-04-25 13:19:11 +0530

    modified for db config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import path from "path";
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
-
+import db from "./config/db";
 import routes from "./routes";
 import isAuth from "./middleware/is-auth";
 
@@ -10,6 +9,9 @@ dotenv.config({path: path.resolve(__dirname, "../.env")});
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// db setup
+db();
 
 // middleware
 app.use(express.json());
@@ -21,20 +23,6 @@ app.use(routes);
 app.get("/", (req, res) => {
     res.json({status: "ok"});
 });
-
-// database setup
-
-mongoose
-    .connect(process.env.DATABASE_URI as string, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("db Connection is successfull");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
 
 app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`);

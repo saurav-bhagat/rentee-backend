03ba45c173e558c1f3f53154320d76c21b8d9895
Ram kumar <rk16013093@gmail.com>
2021-05-09 17:55:30 +0530

    integrating protecting route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import ownerRouter from "./owner.router";
 import tenantRouter from "./tenant.route";
 import authRouter from "./auth.router";
 
+import isAuth from "../middleware/is-auth";
+
 const router: Router = Router();
 
-router.use("/owner", ownerRouter);
-router.use("/tenant", tenantRouter);
+router.use("/owner", isAuth, ownerRouter);
+router.use("/tenant", isAuth, tenantRouter);
 router.use("/auth", authRouter);
 
 export default router;

c75f402e11d936b8e5d076d0af117bc3a4490ca1
Ram kumar <rk16013093@gmail.com>
2021-05-11 21:30:07 +0530

    adding id's in tenant model from user model

--- src/models/tenant/tenant.ts
+++ src/models/tenant/tenant.ts
@@ -1,15 +1,18 @@
 import {Schema, model} from "mongoose";
 import {ITenant} from "./interface";
 
-const tenantSchema = new Schema({
-    userId: {type: Schema.Types.ObjectId, ref: "user"},
-    joinDate: {type: Date, default: Date.now},
-    rentDueDate: Date,
-    securityAmount: Number,
-    ownerId: {type: Schema.Types.ObjectId},
-    roomId: {type: Schema.Types.ObjectId},
-    buildId: {type: Schema.Types.ObjectId},
-});
+const tenantSchema = new Schema(
+    {
+        userId: {type: Schema.Types.ObjectId, ref: "user"},
+        joinDate: {type: Date, default: Date.now},
+        rentDueDate: Date,
+        securityAmount: Number,
+        ownerId: {type: Schema.Types.ObjectId},
+        roomId: {type: Schema.Types.ObjectId},
+        buildId: {type: Schema.Types.ObjectId},
+    },
+    {timestamps: true}
+);
 
 const tenant = model<ITenant>("tenant", tenantSchema);
 

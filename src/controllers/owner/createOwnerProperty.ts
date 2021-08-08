94cae3c0bcf18864e9edc67b8b659e37bb6199ad
ram027 <ram1218290@jmit.ac.in>
2021-08-08 15:56:29 +0530

    fixes #49

--- src/controllers/owner/createOwnerProperty.ts
+++ src/controllers/owner/createOwnerProperty.ts
@@ -5,7 +5,7 @@ import Property from '../../models/property/property';
 import User from '../../models/user/User';
 
 import Maintainer from '../../models/maintainer/maintainer';
-import { formatDbError, isEmptyFields, verifyObjectId } from '../../utils/errorUtils';
+import { formatDbError, verifyObjectId } from '../../utils/errorUtils';
 
 import randomstring from 'randomstring';
 import bcrypt from 'bcrypt';
@@ -17,40 +17,13 @@ import { BasicUser } from './ownerTypes';
 // owner add properties after signup
 export const addOwnerProperty = async (req: Request, res: Response) => {
 	if (req.isAuth) {
-		const { ownerId, buildingsObj, name, email, password } = req.body;
-		const userData = { name, email, password };
+		const { ownerId, buildingsObj } = req.body;
+
 		if (!ownerId || !verifyObjectId([ownerId])) {
 			return res.status(400).json({ err: 'Incorrect owner detail' });
 		}
 
-		if (isEmptyFields(userData)) {
-			return res.status(400).json({ err: 'Either name/email/password is missing' });
-		}
-
-		if (!validator.isEmail(email) || password.length < 6) {
-			return res.status(400).json({ err: 'Either email/password is not valid' });
-		}
-
-		// Updating Onwer basic info
 		try {
-			const salt = await bcrypt.genSalt();
-			const hashedPassword = await bcrypt.hash(password, salt);
-
-			const ownerBasicInfo = await User.findByIdAndUpdate(
-				{ _id: ownerId },
-				{
-					name,
-					email,
-					password: hashedPassword,
-				},
-				{
-					new: true,
-					runValidators: true,
-					context: 'query',
-				}
-			);
-			if (!ownerBasicInfo) return res.status(400).json({ err: 'Invalid owner detail' });
-
 			if (buildingsObj !== undefined && buildingsObj.length !== 0) {
 				// Create property for owner
 				const property = new Property({ ownerId });
@@ -92,7 +65,7 @@ export const addOwnerProperty = async (req: Request, res: Response) => {
 							});
 						} else {
 							// create new maintainer
-							if (email && !validator.isEmail(email)) {
+							if (maintainerEmail && !validator.isEmail(maintainerEmail)) {
 								return res.status(400).json({ err: `Invalid maintainer email ${maintainerEmail}` });
 							}
 							const maintainerPassword = randomstring.generate({ length: 6, charset: 'abc' });
@@ -119,23 +92,25 @@ export const addOwnerProperty = async (req: Request, res: Response) => {
 				}
 				const properties = await property.save();
 
-				const builidingForMaintainer = properties.buildings;
+				// maintainer model getting updated with maintainer-specific details
 
-				for (let i = 0; i < builidingForMaintainer.length; i++) {
-					const builiding = builidingForMaintainer[i];
-					if (builiding.maintainerId) {
-						const maintainerIdInBuilding = builiding.maintainerId;
+				const buildingForMaintainer = properties.buildings;
+
+				for (let i = 0; i < buildingForMaintainer.length; i++) {
+					const building = buildingForMaintainer[i];
+					if (building.maintainerId) {
+						const maintainerIdInBuilding = building.maintainerId;
 
 						// maintainer model getting updated with maintainer-specific details if present.
 						const isMaintainer = await Maintainer.findOneAndUpdate(
 							{ userId: maintainerIdInBuilding },
-							{ $push: { buildings: builiding._id } }
+							{ $push: { buildings: building._id } }
 						);
 
 						// maintainer model getting saved for the first time with maintainer-specific details.
 						if (!isMaintainer) {
 							const buildingIdArray: Array<ObjectId> = [];
-							buildingIdArray.push(builiding._id);
+							buildingIdArray.push(building._id);
 							const doc = {
 								ownerId,
 								userId: maintainerIdInBuilding,
@@ -162,13 +137,8 @@ export const addOwnerProperty = async (req: Request, res: Response) => {
 					}
 					const result = await propertydoc.save();
 
-					return res.status(200).json({ propertyDetails: result });
+					return res.status(200).json({ msg: 'Building details added successfully!' });
 				}
-			}
-			if (ownerBasicInfo) {
-				const { _id, name, email, phoneNumber, userType } = ownerBasicInfo;
-				const updatedUserInfo: BasicUser = { _id, name, email, phoneNumber, userType };
-				return res.status(200).json({ updatedUserInfo });
 			}
 		} catch (error) {
 			return res.status(400).json({ err: formatDbError(error) });
--- src/models/user/User.ts
+++ src/models/user/User.ts
@@ -22,7 +22,6 @@ const userSchema = new Schema(
 		},
 		password: {
 			type: String,
-			required: [true, 'Please enter a password'],
 			minlength: [6, 'Minimum password length is 6 characters'],
 		},
 		phoneNumber: {

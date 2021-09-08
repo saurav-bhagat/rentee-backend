3d993e88ccb59708fa03a13f2d1c1f513e359479
Ram kumar <rk16013093@gmail.com>
2021-09-08 15:58:16 +0530

    change roomNumber type

--- src/models/property/rooms.ts
+++ src/models/property/rooms.ts
@@ -6,7 +6,7 @@ const roomSchema = new Schema(
 		rent: { type: Number, required: [true, 'Please enter room rent'] },
 		type: { type: String, required: [true, 'Please enter room type'] },
 		floor: { type: String, required: [true, 'Please enter floor number'] },
-		roomNo: { type: Number, required: [true, 'Please enter room number'] },
+		roomNo: { type: String, required: [true, 'Please enter room number'] },
 		isEmpty: { type: Boolean },
 		tenants: [{ type: Schema.Types.ObjectId, ref: 'tenant' }],
 	},
